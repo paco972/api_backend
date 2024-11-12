d677b5699e36db08d7efe042773656fd64d97f1f
Pascal PISZYNA <p.piszyna@gmail.com>
2024-11-12 12:12:22 -0400

    Update ciel2025.sql

--- sql/ciel2025.sql
+++ sql/ciel2025.sql
@@ -29,21 +29,5 @@ COMMIT;
 -- Données de la table `etudiant`
 --
 INSERT INTO `etudiant` (`idetudiant`, `nom`, `prenom`, `email`, `telephone`) VALUES
-(1, 'ADENET-LOUVET', 'Bryan', 'bryan.adenetl@gmail.com', '+596696051217'),
-(2, 'BEAUSOLEIL', 'Patrice', 'patricebeausoleil96@gmail.com', '+596696720198'),
-(3, 'CELESTINE', 'Jimmy', 'Jimmycelestine193@gmail.com ', '+596696615883'),
-(5, 'EPHESTION', 'Jean-Eves', 'ephestionjeaneves11@gmail.com', '+596696003764'),
-(6, 'GUILON', 'Gregory', 'gregoryguilon298@gmail.com', '+596696365364'),
-(8, 'LOUIS-ALEXANDRE', 'Laetitia', 'laetitia.louisalexandre@gmail.com', '+596696090308'),
-(9, 'MALCOUSU', 'Emmanuel', 'm.malcousu23@gmail.com', '+596696936600'),
-(10, 'MARIE-SAINTE', 'Moise', 'moise.mariesainte@gmail.com', '+596696982649'),
-(11, 'MICAA', 'Florian', 'micaa.florian7@gmail.com', '+596696737986'),
-(12, 'MONTLOUIS', 'Alhan', 'tisire257@gmail.com', '+596696206114'),
-(13, 'NEJA', 'Sebastien', 'nejsebastian5@gmail.com', '+596696521217'),
-(14, 'PLATOF', 'Nathan', 'platof.nathan6@gmail.com', '+596696010816'),
-(15, 'POMIES', 'Djayron', 'pomies.djayron26@gmail.com', '+596696503446'),
-(16, 'ROGER', 'Bryan', 'Bryanrogerpro@gmail.com', '+596696061183'),
-(17, 'TALLY', 'Noah', 'noah.tally972@gmail.com ', '+596696555714'),
-(18, 'TARPAU', 'Francois-Xavier', 'Travailmatthieu971@gmail.com', '+596696265360'),
-(19, 'TSENG-CHING', 'Enzo', 'enzotc8@gmail.com', '+596696160328'),
-(20, 'VAUDRAN', 'Loann', 'vaudranloann@gmail.com', '');
+(1, 'TERIEUR', 'Alex', 'alex.terieur@gmail.com', '+596696123456),
+(2, 'TERIEUR', 'Alain', 'alain.terieur@gmail.com', '+596696654321');
